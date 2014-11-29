a3a1fddf2256cc6bbfe6c1e874e68a86adf94898
Mike Burr <mburr@unintuitive.org>
2014-11-30 01:05:19 +0900

    update docs

--- pynaut/__init__.py
+++ pynaut/__init__.py
@@ -2,32 +2,35 @@
 """
 ``pynaunt`` allows you to deeply explore and introspect arbitrary python objects.
 
-As an example, we explore the ``os`` module just a bit:
+As an example, we can explore the ``os`` module just a bit:
 
     >>> from pynaut import Container
     >>> import os
     >>> obj = Container(os)
     >>> len(obj.children)
     220
-    >>> pairs = obj.grep_attr_names('wait')
-    >>> len(pairs)
-    12
-    >>> pairs
-    [('wait', <Container(<built-in function wait>) >), ('wait4', <Container(<built-in function wait4>) >), ('wait3',
-    <Container(<built-in function wait3>) >), ('wait', <Container(<built-in function wait>) >), ('wait4', <Container(<built-in
-    function wait4>) >), ('wait3', <Container(<built-in function wait3>) >), ('waitpid', <Container(<built-in function
-    waitpid>) >), ('wait', <Container(<built-in function wait>) >), ('wait4', <Container(<built-in function wait4>) >),
-    ('wait3', <Container(<built-in function wait3>) >), ('waitpid', <Container(<built-in function waitpid>) >),
-    ('waitpid', <Container(<built-in function waitpid>)>)]
-    >>> obj = obj.children['path']
-    >>> obj = obj.children['os']
-    >>> list(obj.ancestry)
-    [<Container(<module 'posixpath' from '/Users/miburr/virtualenv/current/lib/python2.7/posixpath.pyc'>) >,
-     <Container(<module 'os' from '/Users/miburr/virtualenv/current/lib/python2.7/os.pyc'>) >]
+    >>> w = list(obj.grep_attr_names('wait'))
+    >>> len(w)
+    4
+    >>> [a.metadata.name for a in w]
+    ['wait', 'wait4', 'wait3', 'waitpid']
+    >>> from types import ModuleType
+    >>> test = lambda c: isinstance(c.obj, (bool, list, ModuleType))
+    >>> foo = list(obj.get_attr_matches(test))
+    >>> len(foo)
+    20
+    >>> [a.metadata.name for a in foo]
+    ['_copy_reg', 'UserDict', '_abcoll', 'sys', 'py3kwarning', 'meta_path',
+     'path_hooks', 'argv', 'warnoptions', 'path', 'errno', 'path', '__self__',
+     'genericpath', 'stat', 'os', 'warnings', 'filters', 'types', 'linecache']
     >>>
 
 Additionally, there is support for searching an entire object tree for a (name, attribute) pair that meets an arbitrary
 condition.
+
+``pynaut`` also includes a very basic curses based interface (``pynaut_curses``) that lets you explore objects by
+descending through the attribute tree.
+
 """
 
 from main import *
